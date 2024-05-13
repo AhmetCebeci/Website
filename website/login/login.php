55fc39b5f66a0d59884c0da7a0adfbb7450b075e
AhmetCebeci <cebeciahmet10@hotmail.com>
2024-05-13 22:52:25 +0300

    Add files via upload
    bugün şehrim kısmını yaptım ve login phpdeki sorunu ççzdüm.

--- website/login/login.php
+++ website/login/login.php
@@ -1,15 +1,17 @@
-<?
+<?php
 // Form verilerini al
 $email = $_POST['email'];
 $password = $_POST['password'];
 
-if (($email === "b231210077@sakarya.edu.tr"||$email === "B231210077@sakarya.edu.tr" )&& ($password === "b231210077"||$password === "B231210077")) {
+if (($email === "b231210077@sakarya.edu.tr" || $email === "B231210077@sakarya.edu.tr") && ($password === "b231210077" || $password === "B231210077")) {
     // Başarılı giriş, kullanıcıyı karşılama sayfasına yönlendir
-    header("Location: main/main.html");
+    echo "Hoşgeldin b231210077 ";
+    header("refresh:2;url=../main/main.html"); // 2 saniye bekleyip main.html'e yönlendir
     exit();
 } else {
     // Hatalı giriş, kullanıcıyı login sayfasına geri yönlendir
-    header("Location: login.html");
+    echo"Hatalı giriş,lütfen tekrar dene";
+    header("refresh:2; url=login.html");
     exit();
 }
 ?>
